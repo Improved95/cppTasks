57dbcb31987192ab4be0ccfaa041b0e9b9efda7f
e.davydov <e.davydov@g.nsu.ru>
2023-09-08 19:03:36 +0700

    str

--- task1/src/main.cpp
+++ task1/src/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main(int argc, char* argv[]) {
 //    ifstream fileIn(argv[1]);
+//    if (argc < 2) return -1;
     ifstream fileIn("in.txt");
     if (!fileIn) return -1;
-
-
+    string word;
+    fileIn.getLine(word, 1500, ' ');
 
     fileIn.close();
     return 0;
