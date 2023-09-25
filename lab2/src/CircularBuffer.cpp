b55053c5c1a4cec705669a06ba8ff64c0ea66d40
e.davydov <e.davydov@g.nsu.ru>
2023-09-25 22:45:18 +0700

    try add template

--- lab2/src/CircularBuffer.cpp
+++ lab2/src/CircularBuffer.cpp
@@ -1,17 +1,5 @@
 #include "head.h"
-//#include "CircularBuffer.h"
-using std::vector;
-
-template<class T>
-class CircularBuffer {
-private:
-    size_t bufferSize;
-    vector<T> bufferVector; // переименовать, когдас танет понятно на что: на начало буфера или на что-то еще
-
-public:
-    CircularBuffer(size_t bufferSize); // конструктор копирование
-    vector<T> getBufferVector();
-};
+#include "CircularBuffer.h"
 
 template <class T>
 CircularBuffer<T>::CircularBuffer(size_t bufferSize) {
