0fa278a7ae7a40327eef5cb5b44492c5aabdd68c
e.davydov <e.davydov@g.nsu.ru>
2023-09-27 00:19:48 +0700

    add template func

--- lab2/src/CircularBuffer.cpp
+++ lab2/src/CircularBuffer.cpp
@@ -1,11 +1,1 @@
 #include "CircularBuffer.h"
-template <class T>
-CircularBuffer<T>::CircularBuffer(size_t bufferSize) {
-    this->bufferSize = bufferSize;
-    this->bufferVector = vector<int>(bufferSize);
-}
-
-template <class T>
-vector<T> CircularBuffer<T>::getBufferVector() {
-    return this->bufferVector;
-}
--- lab2/src/CircularBuffer.h
+++ lab2/src/CircularBuffer.h
@@ -8,11 +8,22 @@ template<class T>
 class CircularBuffer {
 private:
     size_t bufferSize;
-    vector<T> bufferVector; // переименовать, когдас танет понятно на что: на начало буфера или на что-то еще
 
 public:
-    CircularBuffer(size_t bufferSize); // конструктор копирование
+    vector<T> bufferVector; // переименовать, когдас танет понятно на что: на начало буфера или на что-то еще
+    CircularBuffer(size_t bufferSize); // конструктор создания
     vector<T> getBufferVector();
 };
+
+template <class T>
+CircularBuffer<T>::CircularBuffer(size_t bufferSize) {
+    this->bufferSize = bufferSize;
+    this->bufferVector = vector<int>(bufferSize);
+}
+
+template <class T>
+vector<T> CircularBuffer<T>::getBufferVector() {
+    return this->bufferVector;
+}
 
 #endif
