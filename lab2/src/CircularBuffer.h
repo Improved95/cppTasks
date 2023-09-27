257bd499669ab08eb51a9fcf65b864983437c98c
e.davydov <e.davydov@g.nsu.ru>
2023-09-27 18:54:56 +0700

    add getter by index without checking index

--- lab2/src/CircularBuffer.h
+++ lab2/src/CircularBuffer.h
@@ -3,27 +3,55 @@
 
 #include "head.h"
 using std::vector;
+using namespace std;
 
 template<class T>
 class CircularBuffer {
 private:
-    size_t bufferSize;
+    vector<T> bufferVector; // переименовать, когда станет понятно на что: на начало буфера или на что-то еще
+    size_t capacity;
 
 public:
-    vector<T> bufferVector; // переименовать, когдас танет понятно на что: на начало буфера или на что-то еще
-    CircularBuffer(size_t bufferSize); // конструктор создания
-    vector<T> getBufferVector();
+    CircularBuffer();
+    explicit CircularBuffer(size_t capacity);
+    explicit CircularBuffer(size_t capacity, T elem);
+
+    T & operator[](int i);
+    T & operator[](int i) const;
 };
 
-template <class T>
-CircularBuffer<T>::CircularBuffer(size_t bufferSize) {
-    this->bufferSize = bufferSize;
-    this->bufferVector = vector<int>(bufferSize);
+// всевозможные конструкторы класса
+template<class T>
+CircularBuffer<T>::CircularBuffer() {
+    this->capacity = 0;
+    this->bufferVector = vector<T>(this->capacity);
 }
 
-template <class T>
-vector<T> CircularBuffer<T>::getBufferVector() {
-    return this->bufferVector;
+template<class T>
+CircularBuffer<T>::CircularBuffer(size_t capacity):CircularBuffer() {
+    this->capacity = capacity;
+    this->bufferVector = vector<T>(this->capacity);
 }
+
+template<class T>
+CircularBuffer<T>::CircularBuffer(size_t capacity, T elem):CircularBuffer(capacity) {
+    for (size_t i = 0; i < this->capacity; i++) {
+        this->bufferVector[i] = elem;
+    }
+}
+
+//Доступ по индексу. Не проверяют правильность индекса
+template<class T>
+T & CircularBuffer<T>::operator[](int i) {
+    return this->bufferVector[i];
+}
+
+template<class T>
+T & CircularBuffer<T>::operator[](int i) const {
+    return this->bufferVector[i % this->capacity];
+}
+
+//Доступ по индексу. Методы бросают исключение в случае неверного индекса.
+
 
 #endif
--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,8 +1,12 @@
 #include "CircularBuffer.h"
 using std::cout;
+using std::endl;
+using std::string;
 
 int main() {
-    CircularBuffer<int> cb1(5);
+    CircularBuffer<int> cb1(5, 3);
+
+    cout << cb1[0] << endl;
 
     return 0;
 }
