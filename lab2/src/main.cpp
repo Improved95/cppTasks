52148ff490cf1736a66049fd92a791d0469e5671
e.davydov <e.davydov@g.nsu.ru>
2023-10-05 19:37:51 +0700

    nt close to compl setCap

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -2,11 +2,11 @@
 
 int main() {
     CircularBuffer<int> cb1(7);
-    for (int i = 0; i < 11; i++) {
+    for (int i = 0; i < 6; i++) {
         cb1.push_back(i + 1);
     }
 
-
+    cb1.set_capacity(5);
 
     cout << "" << endl;
 
--- lab2/src/setCapacity.h
+++ lab2/src/setCapacity.h
@@ -1,8 +1,43 @@
 template<class T>
 void CircularBuffer<T>::set_capacity(const size_t newCapacity) {
-    if (newCapacity > capacity) {
-
-    } else {
-
+    if (size == capacity) {
+        indexDecrement(beginPosInBuf, capacity);
     }
+    indexDecrement(endPosInBuf, capacity);
+
+    if (newCapacity > capacity) {
+        if (endPosInBuf >= beginPosInBuf) {
+            beginBufferInMem.resize(newCapacity);
+        } else {
+            beginBufferInMem.resize(newCapacity);
+            for (size_t i = 0; i < capacity - beginPosInBuf; i++) {
+                swapElement(beginBufferInMem[beginPosInBuf + i], beginBufferInMem[newCapacity - beginPosInBuf + i + 1]);
+            }
+        }
+    } else {
+        if (endPosInBuf >= beginPosInBuf) {
+            beginBufferInMem.resize(newCapacity);
+            if (newCapacity > 0) {
+                if (newCapacity - 1 < endPosInBuf) {
+                    endPosInBuf -= endPosInBuf - (newCapacity - 1);
+                    size -= endPosInBuf - (newCapacity - 1);
+                    if (endPosInBuf < beginPosInBuf) {
+                        beginPosInBuf = 0;
+                    }
+                }
+            } else {
+                beginPosInBuf = 0;
+                endPosInBuf = 0;
+            }
+        } else {
+            const size_t diffCap = capacity - newCapacity;
+            if (diffCap )
+        }
+    }
+
+    capacity = newCapacity;
+    if (size == capacity) {
+        indexIncrement(beginPosInBuf, capacity);
+    }
+    indexIncrement(endPosInBuf, capacity);
 }
