a0762048deb24c36aad9fcdaf6eb47738cd7a775
e.davydov <e.davydov@g.nsu.ru>
2023-10-17 01:02:43 +0700

    try add test

--- lab2/src/test.cpp
+++ lab2/src/test.cpp
@@ -1,8 +1,21 @@
 #include "CircularBuffer.h"
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 using circularBuffer::CircularBuffer;
 
-TEST(lab2, firstTest) {
+TEST(lab2, circularBuffer) {
+    CircularBuffer<int> cb1(3);
 
+    for(size_t i = 0; i < cb1.getSize(); i++) {
+        cb1[i] = i;
+    }
+
+    EXPECT_EQ(cb1.getSize(), 3)
+    EXPECT_THAT(cb1.getBeginBufferInMem(), ElementsAre(0, 1, 2));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
 }
