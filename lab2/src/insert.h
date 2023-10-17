21a846fca0d83a0c8ad31ca7cf51386dc161908b
e.davydov <e.davydov@g.nsu.ru>
2023-10-18 00:53:38 +0700

    метод insert неправильно считает индексы после вставки - исправить

--- lab2/src/insert.h
+++ lab2/src/insert.h
@@ -8,22 +8,19 @@ void CircularBuffer<T>::insert(const size_t pos, const T &item) {
                 for (size_t i = 0; i < (endPosInBuf - (beginPosInBuf + pos) + 1); i++) {
                     swapElementInVector(beginBufferInMem[(endPosInBuf - i) % capacity], beginBufferInMem[(endPosInBuf - i + 1) % capacity]);
                 }
-                if (size == capacity - 1) {
+                if ((size + 1) == capacity) {
                     indexIncrement(beginPosInBuf, capacity);
                 }
+                indexIncrement(endPosInBuf, capacity);
+                indexIncrement(endPosInBuf, capacity);
             } else {
                 T elem = beginBufferInMem[(beginPosInBuf + pos) % capacity];
                 for (size_t i = 0; i < (capacity - pos + endPosInBuf + 1); i++) {
                     swapElementInVector(elem, beginBufferInMem[(beginPosInBuf + pos + i + 1) % capacity]);
                 }
-                if (size == capacity) {
-                    indexIncrement(beginPosInBuf, capacity);
-                    indexIncrement(beginPosInBuf, capacity);
-                }
+                indexIncrement(beginPosInBuf, capacity);
             }
             beginBufferInMem[insertPosition] = item;
-            indexIncrement(endPosInBuf, capacity);
-            indexIncrement(endPosInBuf, capacity);
             if (size < capacity) {
                 size++;
             }
