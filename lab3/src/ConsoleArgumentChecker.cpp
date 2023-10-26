95e88e343b1d27114636fdf99197ad9dc00c7e4c
e.davydov <e.davydov@g.nsu.ru>
2023-10-26 11:37:36 +0700

    delete case in consoleargumentchecker

--- lab3/src/ConsoleArgumentChecker.cpp
+++ lab3/src/ConsoleArgumentChecker.cpp
@@ -4,44 +4,46 @@ void ParserConsoleParametersAndInitialGame::parseAndInitial(int argc, char **arg
     ifstream fileIn;
     ofstream fileOut;
     size_t tickQuantity;
-
-
-    switch(argc) {
-        case 4:
-            fileOut.open(argv[3]);
-            if (!fileOut.is_open()) {
-                cout << "Incorrect output file path. You need enter path to input file, quantity ticks, path to output file. Example: 'fileIn.life' 5 'fileOut.life'" << endl;
-                gameObject->gameWithoutInitialData();
-                break;
-            }
-            try {
-                tickQuantity = std::stoull(argv[2]);
-                if (tickQuantity == 0ULL) {
-                    throw exception();
-                }
-            } catch (const exception &ex) {
-//                cout << ex.what() << endl;
-                cout << "Incorrect output file path. You need enter path to input file, quantity ticks, path to output file. Example: 'fileIn.life' 5 'fileOut.life'" << endl;
-                gameObject->gameWithoutInitialData();
-                break;
-            }
-        case 2:
-            fileIn.open(argv[1]);
-            if (!fileIn.is_open()) {
-                cout << "Incorrect output file path. You need enter path to input file, quantity ticks, path to output file. Example: 'fileIn.life' 5 'fileOut.life'" << endl;
-                gameObject->gameWithoutInitialData();
-                break;
-            }
-            if (argc == 2) {
-                gameObject->gameWithInitialData(fileIn);
-            } else if (argc == 4) {
-                gameObject->offlineGame(fileIn, tickQuantity, fileOut);
-            }
-            break;
-        default:
-            cout << "Incorrect input. You need enter path to input file, quantity ticks, path to output file. Example: 'fileIn.life' 5 'fileOut.life'" << endl;
-            break;
+    if (argc == 2) {
+        fileIn.open(argv[1]);
+        if (!fileIn.is_open()) {
+            cout << "Incorrect output file path. You need enter path to input file, quantity ticks, path to output file. Example: 'fileIn.life' 5 'fileOut.life'" << endl;
+            gameObject->gameWithoutInitialData();
+            goto END_FUNCTIONS_AND_CLOSE_FILES;
+        }
     }
+    if (argc == 4) {
+        fileOut.open(argv[3]);
+        if (!fileOut.is_open()) {
+            cout << "Incorrect output file path. You need enter path to input file, quantity ticks, path to output file. Example: 'fileIn.life' 5 'fileOut.life'" << endl;
+            gameObject->gameWithoutInitialData();
+            goto END_FUNCTIONS_AND_CLOSE_FILES;
+        }
+        try {
+            tickQuantity = std::stoull(argv[2]);
+            if (tickQuantity == 0ULL) {
+                throw exception();
+            }
+        } catch (const exception &ex) {
+//            cout << ex.what() << endl;
+            cout << "Incorrect output file path. You need enter path to input file, quantity ticks, path to output file. Example: 'fileIn.life' 5 'fileOut.life'" << endl;
+            gameObject->gameWithoutInitialData();
+            goto END_FUNCTIONS_AND_CLOSE_FILES;
+        }
+    }
+    if (argc != 2 && argc != 4) {
+        cout << "Incorrect input. You need enter path to input file, quantity ticks, path to output file. Example: 'fileIn.life' 5 'fileOut.life'" << endl;
+        gameObject->gameWithoutInitialData();
+        goto END_FUNCTIONS_AND_CLOSE_FILES;
+    }
+
+    if (argc == 2) {
+        gameObject->gameWithInitialData(fileIn);
+    } else if (argc == 4) {
+        gameObject->offlineGame(fileIn, tickQuantity, fileOut);
+    }
+
+    END_FUNCTIONS_AND_CLOSE_FILES:
     fileIn.close();
     fileOut.close();
 }
