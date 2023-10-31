ddfd16ac25a2f19184d850cb141b2f29ce07a22e
e.davydov <e.davydov@g.nsu.ru>
2023-10-31 14:19:27 +0700

    some improve

--- lab3/src/BlockOfCells.cpp
+++ lab3/src/BlockOfCells.cpp
@@ -1,26 +1,20 @@
 #include "Life.h"
-
-BlockOfCells::
-
+#define TWO 2
 BlockOfCells::BlockOfCells() {
     left = nullptr;
     right = nullptr;
 }
 
 BlockOfCells * BlockOfCells::constructorOfStruct(const size_t rows, const size_t columns, const size_t blockSize, const int mode) {
-//    const size_t binary = 2;
-//    const size_t quantBlocksInRow = (rows / binary) + (rows % binary);
-//    const size_t quantBlocksInColums = (columns / binary) + (columns % binary);
-
     if (mode == 0) {
-        if (rows / 2 >= blockSize) {
-            this->left = constructorOfStruct(rows / 2, columns, blockSize, 1);
-            this->right = constructorOfStruct(rows / 2, columns, blockSize, 1);
+        if (rows / TWO >= blockSize) {
+            this->left = constructorOfStruct(rows / TWO, columns, blockSize, 1);
+            this->right = constructorOfStruct(rows / TWO, columns, blockSize, 1);
         }
     } else {
-        if (columns / 2 >= blockSize) {
-            this->left = constructorOfStruct(rows, columns / 2, blockSize, 0);
-            this->right = constructorOfStruct(rows, columns / 2, blockSize, 0);
+        if (columns / TWO >= blockSize) {
+            this->left = constructorOfStruct(rows, columns / TWO, blockSize, 0);
+            this->right = constructorOfStruct(rows, columns / TWO, blockSize, 0);
         }
     }
     BlockOfCells *ptrBlock = new BlockOfCells();
--- lab3/src/ExceptionHandling.cpp
+++ lab3/src/ExceptionHandling.cpp
@@ -1,14 +1,18 @@
 #include "Life.h"
 
+/*выдает модуль числа*/
 bool ExceptionHandling::strtoullIsCorrect(size_t &value, const string &input, const char *message) {
     size_t numCharsConverted = 0;
     string newInput = input;
 
+    // обработка отрицательных чисел
     size_t newInputSize = newInput.size();
     if (input[0] == '-') {
         newInput = input.substr(1, input.size());
         newInputSize = newInput.size();
+
     }
+
     try {
         value = std::stoull(newInput, &numCharsConverted);
         if (numCharsConverted != newInputSize) {
