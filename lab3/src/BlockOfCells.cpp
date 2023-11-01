c0753cea18e60b25c27b0f85d42c1a14f65deb3d
e.davydov <e.davydov@g.nsu.ru>
2023-11-01 18:40:54 +0700

    delete commentaries

--- lab3/src/BlockOfCells.cpp
+++ lab3/src/BlockOfCells.cpp
@@ -8,24 +8,6 @@ BlockOfCells::BlockOfCells() {
 }
 
 BlockOfCells * BlockOfCells::constructorOfStruct(BlockOfCells *node, const size_t rows, const size_t columns, const size_t blockSize, const int mode) {
-//    if (mode == 0) {
-//        if (rows / DIVIDER >= blockSize) {
-//            block->left = new BlockOfCells();
-//            constructorOfStruct(block->left, rows / DIVIDER, columns, blockSize, 1);
-//            block->right = new BlockOfCells();
-//            constructorOfStruct(block->right, rows / DIVIDER, columns, blockSize, 1);
-//        }
-//    } else {
-//        if (columns / DIVIDER >= blockSize) {
-//            block->left = new BlockOfCells();
-//            constructorOfStruct(block->left, rows, columns / DIVIDER, blockSize, 0);
-//            block->right = new BlockOfCells();
-//            constructorOfStruct(block->right, rows, columns / DIVIDER, blockSize, 0);
-//        }
-//    }
-//    set<Cell> setObj;
-//    this->cellsSet = &setObj;
-
     if (mode == 0) {
         if (rows / DIVIDER >= blockSize) {
             BlockOfCells *leftNode = new BlockOfCells();
