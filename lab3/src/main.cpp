f45d972135e86fe6c33ae0cb78f0206652e1ef2d
e.davydov <e.davydov@g.nsu.ru>
2023-11-11 00:43:23 +0700

    основной рабочий вариант

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -1,21 +1,21 @@
 #include "Life.h"
-#include <fstream>
+//#include <fstream>
 
 int main(int argc, char **argv) {
-    ifstream fileIn;
-    fileIn.open("base.life");
-    if (!fileIn.is_open()) return 1;
+//    ifstream fileIn;
+//    fileIn.open("base.life");
+//    if (!fileIn.is_open()) return 1;
 
-    ofstream fileOut;
-    fileOut.open("out.life");
-    if (!fileIn.is_open()) return 1;
+//    ofstream fileOut;
+//    fileOut.open("out.life");
+//    if (!fileIn.is_open()) return 1;
 
     Life life1;
     life1.initialFieldWithConsoleParameters(argc, argv);
 //    life1.initialField(fileIn);
 //    life1.initialField(fileIn, 28, fileOut);
 
-    fileIn.close();
-    fileOut.close();
+//    fileIn.close();
+//    fileOut.close();
     return 0;
 }
