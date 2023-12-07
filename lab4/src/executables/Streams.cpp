30d62b39f51012881fbccbdef3a460c46e3d3a31
e.davydov <e.davydov@g.nsu.ru>
2023-12-08 00:13:39 +0700

    добавил библиотеку для работы с wav файлами

--- lab4/src/executables/Streams.cpp
+++ lab4/src/executables/Streams.cpp
@@ -49,60 +49,7 @@ char *BinaryStreamIn::getSamplesInOneSecond(const size_t second, const size_t fr
 
 int BinaryStreamIn::parseMetadataInWavFile(const size_t frequency, const size_t bitsPerSample,
                                            const size_t numberOfChannels, const size_t compressingCode) {
-    /*эксепшоны в этом методе не работают, поэтому здесь не будет,
-     * здесь вообще ничего не работает, если быть честным, strcmp тоже не работает*/
-    this->metadataSize = 8;
-
-    //RIFF
-    char data[4];
-    this->stream.read(data, 4);
-
-    if (strcmp(data, "RIFF") != 0) { return 8; }
-
-    //размер файла
-    this->stream.read(data, 4);
-    unsigned int fileSize =  *reinterpret_cast<int*>(data) + 8;
-
-    //WAVE
-    this->stream.read(data, 4);
-    if (strcmp(data, "WAVE") != 0) { return 8; }
-
-    //fmt
-    this->stream.read(data, 4);
-    if (strcmp(data, "fmt ") != 0) { return 8; }
-
-    //размер данных блока fmt
-    this->stream.read(data, 4);
-    this->metadataSize += *reinterpret_cast<int*>(data);
-
-    //код сжатия
-    this->stream.read(data, 2);
-    int dataInt = *reinterpret_cast<int*>(data);
-    if (dataInt != compressingCode) { return 8; }
-
-    //количество каналов
-    this->stream.read(data, 2);
-    dataInt = *reinterpret_cast<int*>(data);
-    if (dataInt != compressingCode) { return 8; }
-
-    //частота дискретизации
-    this->stream.read(data, 4);
-    dataInt = *reinterpret_cast<int*>(data);
-    if (dataInt != frequency) { return 8; }
-
-    //количество байт в секунде
-    this->stream.read(data, 4);
-    dataInt = *reinterpret_cast<int*>(data);
-    if (dataInt != frequency * bitsPerSample / BITS_PER_BYTE) { return 8; }
-
-    /*в первых двух байтах лежит некий размер блока, а во вторых двух
-     * лежит количество значащих бит на выборку, чтобы это все не значило*/
-    this->stream.read(data, 4);
-
-    //list или data
-    this->stream.read(data, 4);
-    if (strcmp(data, "data") != 0 && strcmp(data, "LIST") != 0) { return 8; }
-
-    this->stream.seekg(0, this->stream.beg);
+    
+    
     return 0;
 }
